0acb2dfab0693b03f3b772a65ea05bfcc33f88f6
rKAmindo <randykamindo@gmail.com>
2023-02-02 18:21:38 -0500

    Implement Merge Sort from HackerRank

--- src/algorithms/Driver.java
+++ src/algorithms/Driver.java
@@ -8,7 +8,7 @@ import java.util.Arrays;
 public class Driver {
     public static void main(String[] args) {
         int[] nums = {3, 13, -2, 55, 9, 4};
-        QuickSort.quickSort(nums);
+        MergeSort.mergeSort(nums);
         System.out.println(Arrays.toString(nums));
     }
 }
--- src/algorithms/MergeSort.java
+++ src/algorithms/MergeSort.java
@@ -2,67 +2,44 @@ package algorithms;
 
 public class MergeSort {
 
-    private  static void merge(int[] arr, int l, int m, int r) {
-
-        // Find sizes of two subarrays to be merged
-        int n1 = m - l + 1;
-        int n2 = r - m;
-
-        // Create temp arrays
-        int L[] = new int[n1];
-        int R[] = new int[n2];
-
-        // Copy data from temp arrays
-        for (int i = 0; i < n1; i++)
-            L[i] = arr[l + i];
-        for (int j = 0; j < n2; j++)
-            R[j] = arr[m + 1 + j];
-
-        // Merge the temp arrays
-
-        // Initial indexes of the first and second subarrays
-        int i = 0, j = 0;
-
-        // Initial index of merged subarray array
-        int k = l;
-        while (i < n1 && j < n2) {
-            if (L[i] <= R[j]) {
-                arr[k] = L[i];
-                i++;
-            }
-            else {
-                arr[k] = R[j];
-                j++;
-            }
-            k++;
-        }
-
-        // Copy remaining elements of L[] if any
-        while (i < n1) {
-            arr[k] = L[i];
-            i++;
-            k++;
-        }
-
-        // Copy remaining elements of R[] if any
-        while (j < n2) {
-            arr[k] = R[j];
-            j++;
-            k++;
-        }
+    public static void mergeSort(int[] array) {
+        mergeSort(array, new int[array.length], 0, array.length - 1);
     }
 
-    public static void sort(int[] arr, int l, int r) {
-        if (l < r) {
-            // Find the middle point
-            int m = l + (r - l) / 2;
+    public static void mergeSort(int[] array, int[] temp, int leftStart, int rightEnd) {
 
-            // Sort first and second halves
-            sort(arr, l, m);
-            sort(arr, m + 1, r);
+        // out of bounds
+        if (leftStart >= rightEnd)
+            return;
+        int middle = (leftStart + rightEnd) / 2;
+        mergeSort(array, temp, leftStart, middle);
+        mergeSort(array, temp, middle + 1, rightEnd);
+        mergeHalves(array, temp, leftStart, rightEnd);
+    }
 
-            // Merge the sorted halves
-            merge(arr, l, m, r);
+    public static void mergeHalves(int[] array, int[] temp, int leftStart, int rightEnd) {
+        int leftEnd = (rightEnd + leftStart) / 2;
+        int rightStart = leftEnd + 1;
+        int size = rightEnd - leftStart + 1;
+
+        int left = leftStart;
+        int right = rightStart;
+        int index = leftStart;
+
+        // insert elements into temp array
+        while (left <= leftEnd && right <= rightEnd) {
+            if (array[left] <= array[right]) {
+                temp[index] = array[left];
+                left++;
+            } else {
+                temp[index] = array[right];
+                right++;
+            }
+            index++;
         }
+
+        System.arraycopy(array, left, temp, index, leftEnd - left + 1);
+        System.arraycopy(array, right, temp, index, rightEnd - right + 1);
+        System.arraycopy(temp, leftStart, array, leftStart, size);
     }
 }
